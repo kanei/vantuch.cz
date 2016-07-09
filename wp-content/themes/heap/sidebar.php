cbf53113ddf693388192277c79763e6d130a28dd
root <root@nginx-b6ee6f56.c.bitnami-builder.internal>
2016-07-09 03:28:00 +0000

    remove themes

--- wp-content/themes/heap/sidebar.php
+++
@@ -1,5 +0,0 @@
-<?php if (is_active_sidebar('sidebar-main')): ?>
-		<aside class="sidebar  sidebar--main">
-		<?php dynamic_sidebar('sidebar-main'); ?>
-		</aside><!-- .sidebar -->
-<?php endif; ?>
--- wp-content/themes/heap/theme-partials/wpgrade-partials/google-fonts-config.php
+++
@@ -1,8 +0,0 @@
-<?php defined('ABSPATH') or die;
-	/* @var array $families */
- ?>
-<script type="text/javascript">
-	WebFont.load ({
-		google : { families: <?php echo json_encode($families); ?> }
-	});
-</script>
