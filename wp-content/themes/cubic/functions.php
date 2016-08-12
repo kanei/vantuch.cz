fcd8365e3a1b6be037c3d7c761b4fe6b4d14fbb9
Marek Vantuch <marek.vantuch@gmail.com>
2016-08-12 20:33:21 +0900

    Shop styling

--- wp-content/themes/cubic/functions.php
+++ wp-content/themes/cubic/functions.php
@@ -8,15 +8,20 @@
 /**
  * WooComerce integration
  */
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 
 add_action('woocommerce_before_main_content', 'cubic_before_main_content', 10);
 add_action('woocommerce_after_main_content', 'cubic_after_main_content', 10);
 
 add_action('woocommerce_before_single_product', 'cubic_before_single_product', 10);
+add_action('woocommerce_after_single_product', 'cubic_after_single_product', 10);
 
 add_action('after_setup_theme', 'cubic_woocommerce_support' );
+
+add_action('init', 'cubic_remove_wc_breadcrumbs' );
+
+add_filter('woocommerce_show_page_title', 'cubic_hide_page_title' );
 
 function cubic_before_main_content() {
 	echo '<div id="primary" class="content-area">
@@ -31,6 +36,38 @@ function cubic_after_main_content() {
 function cubic_woocommerce_support() {
 	add_theme_support( 'woocommerce' );
 }
+
+function cubic_before_single_product() {
+  echo '<div class="entry-content">';
+}
+
+function cubic_after_single_product() {
+  echo '</div>';
+}
+
+function cubic_remove_wc_breadcrumbs() {
+  remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
+}
+
+function cubic_hide_page_title() {
+  return false;
+}
+
+/* Product loop */
+
+add_action('woocommerce_before_shop_loop_item', 'cubic_before_shop_loop_item', 10);
+add_action('woocommerce_after_shop_loop_item', 'cubic_after_shop_loop_item', 10);
+
+function cubic_before_shop_loop_item() {
+  echo '<div class="entry-thumbnail">';
+}
+
+function cubic_after_shop_loop_item() {
+  echo '</div>';
+}
+
+remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10);
+
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
